ef77e075add93dd1d864a29358f53332f823ca3d
samed <ss.sametsever@gmail.com>
2026-08-13 22:46:40 +0300

    add blending while i think nobody using it

--- VANILLA_WINDOW/v1.0.0/src/vanilla_window_vulkan_pipeline.c
+++ VANILLA_WINDOW/v1.0.0/src/vanilla_window_vulkan_pipeline.c
@@ -91,7 +91,13 @@ int VANILLA_VK_createPipeline(void) {
     };
 
     VkPipelineColorBlendAttachmentState colorBlendAttachment = {
-        .blendEnable = VK_FALSE,
+        .blendEnable = VK_TRUE,
+        .srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA,
+        .dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
+        .colorBlendOp = VK_BLEND_OP_ADD,
+        .srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE,
+        .dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO,
+        .alphaBlendOp = VK_BLEND_OP_ADD,
         .colorWriteMask =
             VK_COLOR_COMPONENT_R_BIT |
             VK_COLOR_COMPONENT_G_BIT |
